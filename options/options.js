354f863944f2191286e57dc08d3b52a3e5e117b8
Crowdbotics <team@crowdbotics.com>
2023-10-26 01:50:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dc-golf-academy-44149",
+  url: "https://dc-golf-academy-44149.botics.co",
+  api: "https://dc-golf-academy-44149.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
